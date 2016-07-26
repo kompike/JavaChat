be1621125a686ef191531a3efabff5a2f2ac5489
kompike <kompanietskostya@gmail.com>
2016-07-27 01:20:05 +0300

    Web module: Dispatcher servlet added

---
+++ webapp/src/main/java/com/javaclasses/webapp/HandlerRegistry.java
@@ -0,0 +1,9 @@
+package com.javaclasses.webapp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HandlerRegistry {
+
+    private final Map<String, String> registry = new HashMap<>();
+}
