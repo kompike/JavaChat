abcbd28f9bc4f32051268df1b4bec515a57b075f
kompike <kompanietskostya@gmail.com>
2016-07-27 18:14:42 +0300

    Web module: Test for dispatcher servlet added

---
+++ webapp/src/test/java/com/javaclasses/webapp/DispatcherServletShould.java
@@ -0,0 +1,55 @@
+package com.javaclasses.webapp;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.apache.http.HttpHeaders.USER_AGENT;
+import static org.junit.Assert.assertTrue;
+
+public class DispatcherServletShould {
+
+    @Test
+    public void allowToRegisterNewUser() throws IOException {
+
+        String url = "http://localhost:8080/register";
+
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpPost post = new HttpPost(url);
+
+        post.setHeader("User-Agent", USER_AGENT);
+
+        List<NameValuePair> urlParameters = new ArrayList<>();
+        urlParameters.add(new BasicNameValuePair("nickname", "nickname_" + (Math.random() * 25)));
+        urlParameters.add(new BasicNameValuePair("password", "password"));
+        urlParameters.add(new BasicNameValuePair("confirmPassword", "password"));
+
+        post.setEntity(new UrlEncodedFormEntity(urlParameters));
+
+        HttpResponse response = client.execute(post);
+
+        BufferedReader rd = new BufferedReader(
+                new InputStreamReader(response.getEntity().getContent()));
+
+        StringBuilder result = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+
+        System.out.println(result.toString());
+
+        assertTrue("", !result.toString().isEmpty());
+    }
+}
