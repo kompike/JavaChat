28e6098b19667cc938b070917e39db2643560e09
kompike <kompanietskostya@gmail.com>
2016-07-28 19:07:33 +0300

    Chat service: Added tests for joining/leaving chat

--- model/src/test/java/com/javaclasses/chat/model/ChatServiceShould.java
+++ model/src/test/java/com/javaclasses/chat/model/ChatServiceShould.java
@@ -1,22 +1,23 @@
 package com.javaclasses.chat.model;
 
 import com.javaclasses.chat.model.dto.ChatDTO;
+import com.javaclasses.chat.model.dto.RegistrationDTO;
 import com.javaclasses.chat.model.entity.tinytype.ChatId;
 import com.javaclasses.chat.model.entity.tinytype.ChatName;
 import com.javaclasses.chat.model.entity.tinytype.UserId;
-import com.javaclasses.chat.model.service.ChatCreationException;
-import com.javaclasses.chat.model.service.ChatService;
+import com.javaclasses.chat.model.service.*;
 import com.javaclasses.chat.model.service.impl.ChatServiceImpl;
+import com.javaclasses.chat.model.service.impl.UserServiceImpl;
 import org.junit.Test;
 
-import static com.javaclasses.chat.model.service.ErrorMessage.CHAT_ALREADY_EXISTS;
-import static com.javaclasses.chat.model.service.ErrorMessage.CHAT_NAME_CANNOT_BE_EMPTY;
+import static com.javaclasses.chat.model.service.ErrorMessage.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class ChatServiceShould {
 
     private final ChatService chatService = ChatServiceImpl.getInstance();
+    private UserService userService = UserServiceImpl.getInstance();
 
     @Test
     public void allowToCreateNewChat() throws ChatCreationException {
@@ -92,6 +93,115 @@ public class ChatServiceShould {
                     CHAT_ALREADY_EXISTS.toString(), ex.getMessage());
 
             chatService.delete(chatId);
+        }
+    }
+
+    @Test
+    public void allowUserToJoinChat()
+            throws ChatJoiningException, ChatCreationException, UserRegistrationException {
+
+        final String chatName = "NewChat";
+        final String nickname = "User";
+        final String password = "password";
+
+        final UserId userId = userService.register(new RegistrationDTO(nickname, password, password));
+
+        final ChatId chatId = chatService.createChat(userId, new ChatName(chatName));
+        final ChatDTO chatDTO = chatService.joinChat(userId, chatId);
+
+        assertEquals("User did not join the chat.",
+                1, chatDTO.getUserNameList().size());
+
+        try {
+            chatService.joinChat(userId, chatId);
+            fail("User joined the chat he already joined.");
+        } catch (ChatJoiningException ex) {
+            assertEquals("Wrong message for joining chat which user already joined.",
+                    USER_ALREADY_JOINED.toString(), ex.getMessage());
+
+            chatService.delete(chatId);
+            userService.delete(userId);
+        }
+
+    }
+
+    @Test
+    public void prohibitUserToJoinChatHeAlreadyJoined()
+            throws UserRegistrationException, ChatCreationException, ChatJoiningException {
+
+        final String chatName = "NewChat";
+        final String nickname = "User";
+        final String password = "password";
+
+        final UserId userId = userService.register(new RegistrationDTO(nickname, password, password));
+
+        final ChatId chatId = chatService.createChat(userId, new ChatName(chatName));
+        final ChatDTO chatDTO = chatService.joinChat(userId, chatId);
+
+        assertEquals("User did not join the chat.",
+                1, chatDTO.getUserNameList().size());
+
+        chatService.delete(chatId);
+        userService.delete(userId);
+
+    }
+
+    @Test
+    public void allowUserToLeaveChat()
+            throws UserRegistrationException, ChatCreationException, ChatJoiningException, ChatLeavingException {
+
+        final String chatName = "NewChat";
+        final String nickname = "User";
+        final String password = "password";
+
+        final UserId userId = userService.register(new RegistrationDTO(nickname, password, password));
+
+        final ChatId chatId = chatService.createChat(userId, new ChatName(chatName));
+        final ChatDTO chatDTO = chatService.joinChat(userId, chatId);
+
+        assertEquals("User did not join the chat.",
+                1, chatDTO.getUserNameList().size());
+
+        final ChatDTO chatWithoutUsers = chatService.leaveChat(userId, chatId);
+
+        assertEquals("User did not leave the chat.",
+                0, chatWithoutUsers.getUserNameList().size());
+
+        chatService.delete(chatId);
+        userService.delete(userId);
+
+    }
+
+    @Test
+    public void prohibitUserToLeaveChatHeAlreadyLeft()
+            throws UserRegistrationException, ChatCreationException, ChatJoiningException, ChatLeavingException {
+
+        final String chatName = "NewChat";
+        final String nickname = "User";
+        final String password = "password";
+
+        final UserId userId = userService.register(new RegistrationDTO(nickname, password, password));
+
+        final ChatId chatId = chatService.createChat(userId, new ChatName(chatName));
+        final ChatDTO chatDTO = chatService.joinChat(userId, chatId);
+
+        assertEquals("User did not join the chat.",
+                1, chatDTO.getUserNameList().size());
+
+        final ChatDTO chatWithoutUsers = chatService.leaveChat(userId, chatId);
+
+        assertEquals("User did not leave the chat.",
+                0, chatWithoutUsers.getUserNameList().size());
+
+        try {
+            chatService.leaveChat(userId, chatId);
+            fail("User left the chat he already left.");
+        } catch (ChatLeavingException ex) {
+            assertEquals("Wrong message for leaving chat which user already left.",
+                    USER_ALREADY_LEFT.toString(), ex.getMessage());
+
+            chatService.delete(chatId);
+            userService.delete(userId);
         }
     }
 }
